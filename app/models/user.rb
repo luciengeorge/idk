f81f5de77f06793694f61ecaf6081b15fd3a1fd6
Lucien George <luciengeorge95@gmail.com>
2018-11-19 18:12:23 +0000

    inbox working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   has_many :wishes
   has_many :activities, through: :wishes
   has_many :events, through: :hostings
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
