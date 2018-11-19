5b1319cc6033d1fa528d1c7f2e89a277eb8c0cc9
Izzy Brown <izzybrown@hotmail.co.uk>
2018-11-19 16:54:43 +0000

    categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,9 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Categories.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :categories
+  resources :categories, only: [:index, :show]
   resources :events, except: [:new, :create]
-  resources :activities, only: [:show, :index] do
+  resources :activities, only: [:show] do
     resources :events, only: [:new, :create]
     resources :wishes
   end
