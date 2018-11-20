07f995bb5ae626a09447f6d4a6f7a6dac3745162
Lucien George <luciengeorge95@gmail.com>
2018-11-20 11:02:14 +0000

    schema modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_143322) do
+ActiveRecord::Schema.define(version: 2018_11_20_105915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,14 @@ ActiveRecord::Schema.define(version: 2018_11_19_143322) do
     t.text "description"
     t.bigint "category_id"
     t.datetime "date"
-    t.float "price"
+    t.string "price"
     t.string "photo"
     t.string "location"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
+    t.string "hours"
     t.index ["category_id"], name: "index_activities_on_category_id"
   end
 
