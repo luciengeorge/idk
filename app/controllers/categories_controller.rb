2d092f824be7b0e74431e4f54bd3b7df1d1d7a6a
Ife Odugbesan <ifeodugbesan@yahoo.com>
2018-11-21 17:34:58 +0000

    created the search filters

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,5 +7,16 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @activities = Activity.where(category: @category)
+    # @activities = Activity.joins(:tags).where(category: @category).where("tags.name ILIKE ?", params[:vibe])
+    search_filter
+  end
+
+  private
+
+  def search_filter
+    if params[:number_of_people].present? || params[:who_are_you_with].present? || params[:location].present? || params[:vibe].present?
+      search = params[:number_of_people] + " " + params[:who_are_you_with] + " " + params[:location] + " " + params[:vibe]
+      @activities = Activity.search_by_activity_info(search)
+    end
   end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,4 +5,14 @@ class Activity < ApplicationRecord
   has_many :tags, through: :activity_tags
   has_many :wishes, dependent: :destroy
   has_many :users, through: :wishes
+
+  include PgSearch
+  pg_search_scope :search_by_activity_info,
+    against: [ :location ],
+     associated_against: {
+      tags: [ :name]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
