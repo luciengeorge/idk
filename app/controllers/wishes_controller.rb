12ac2eca6930228931820ad082364733b3608f7a
Lucien George <luciengeorge95@gmail.com>
2018-11-21 19:37:59 +0000

    working on ajax for creating and deleting wish from category show page

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,17 +1,45 @@
 class WishesController < ApplicationController
+  before_action :find_activity, only: %i[create destroy]
   def index
     @wish = Wish.where(user: current_user)
     # @event = Event.all
   end
 
   def create
-    Wish.create(user: current_user, activity: Activity.find(params[:activity_id]))
-    redirect_to category_path(Activity.find(params[:activity_id]).category)
+    wish = Wish.new(user: current_user, activity: @activity)
+    if wish.save!
+      respond_to do |format|
+        format.html { redirect_to category_path(@activity.category) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'categories/show' }
+        format.js
+      end
+    end
+    # redirect_to category_path(Activity.find(params[:activity_id]).category)
   end
 
   def destroy
     @wish = Wish.find(params[:id])
-    @wish.delete
-    redirect_to wishes_path
+    if @wish.delete!
+      respond_to do |format|
+        format.html { redirect_to wish_path(@wish) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'categories/show' }
+        format.js
+      end
+    end
+    # redirect_to wishes_path
+  end
+
+  private
+
+  def find_activity
+    @activity = Activity.find(params[:activity_id])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Idk
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
