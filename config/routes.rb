3209bf5be24ba4789a2494577340da95de0d42f9
Izzy Brown <izzybrown@hotmail.co.uk>
2018-11-21 13:57:15 +0000

    wheel responsive

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   end
+
+  post '/', to: 'pages#wheel'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
