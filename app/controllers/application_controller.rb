9a8c9f63e7a964a9398e70b6e7e28cf0c054ccb6
Ife Odugbesan <ifeodugbesan@yahoo.com>
2018-11-22 19:09:32 +0000

    styled sign in and sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,8 +31,8 @@ class ApplicationController < ActionController::Base
     store_location_for(:user, request.fullpath)
   end
   def home
-    forecast = JSON.parse(open("https://api.darksky.net/forecast/fb6e29ca175e8558227b0cc4d2ea70f0/51.5074,0.1278").read)
-    days_array = forecast['daily']['data']
-    @today = days_array[0]
+    # forecast = JSON.parse(open("https://api.darksky.net/forecast/fb6e29ca175e8558227b0cc4d2ea70f0/51.5074,0.1278").read)
+    # days_array = forecast['daily']['data']
+    # @today = days_array[0]
   end
 end
