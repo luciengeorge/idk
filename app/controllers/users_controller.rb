24adfd4a000d36549fde54f083b9c691c8790fab
Lucien George <luciengeorge95@gmail.com>
2018-11-26 12:29:17 +0000

    added profile info on users pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @events = @user.events
     redirect_to profile_path if current_user == @user
   end
 end
