eb54feab6da1ada1e4d6330f1597ef71738c70eb
Ife Odugbesan <ifeodugbesan@yahoo.com>
2018-11-26 15:28:05 +0000

    added calender

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,7 +12,7 @@ class CategoriesController < ApplicationController
 
   def search_filter
     if params[:number_of_people].present? || params[:who_are_you_with].present? || params[:location].present? || params[:vibe].present?
-      search = params[:number_of_people] + " " + params[:who_are_you_with] + " " + params[:location] + " " + params[:vibe]
+      search = params[:number_of_people].downcase + " " + params[:who_are_you_with].downcase + " " + params[:location].downcase + " " + params[:vibe].downcase
       @activities = Activity.search_by_activity_info(search).where(category: @category)
     end
   end
