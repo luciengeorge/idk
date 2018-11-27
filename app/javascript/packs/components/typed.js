f853c00a1f49d7b31fa70d7fb7f780d5f2593103
Gabriela Martinez <gmartinezcampos@mac.com>
2018-11-27 18:41:09 +0000

    created some typed js friends

---
+++ app/javascript/packs/components/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+function loadDynamicBannerText() {
+  new Typed('#banner-typed-text', {
+    strings: ["Spin that wheel", "Feeling indecisive?", "Break the loop"],
+    typeSpeed: 70,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
