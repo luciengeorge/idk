61b6947619b05f7ad89b66fd917e2b9d296b9e60
Lucien George <luciengeorge95@gmail.com>
2019-10-19 19:05:42 +0100

    test mailer

---
+++ app/mailers/test_mailer.rb
@@ -0,0 +1,13 @@
+class TestMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.test_mailer.test.subject
+  #
+  def test
+    @greeting = "Hi"
+
+    mail to: "contact@app-idk.com"
+  end
+end
